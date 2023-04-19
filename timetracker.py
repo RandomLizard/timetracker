6cf6d5d5f3fcc3b555ef4168b9db4081b074c662
Tate Wilson <tatekwilson.gdi@gmail.com>
2023-04-18 22:58:08 -0700

    Part way through making TimeManager it's own class

--- timetracker.py
+++ timetracker.py
@@ -4,83 +4,109 @@ import os
 import datetime as dt
 import csv
 
-def return_current_time():
-    now = dt.datetime.now()
-    current_time.config(text = now.strftime('%I:%M:%S %p'))
-    current_time.after(1000, return_current_time)
+class TimeManager():
 
-def begin_task():
-    global start_time
-    start['state'] = 'disabled'
-    stop['state'] = 'active'
-    pause['state'] = 'active'
-    start_time = dt.datetime.now()
-    task_category_combobox['state'] = 'disabled'
-    task_description_entry['state'] = 'disabled'
-    name_entry['state'] = 'disabled'
-    current_time.config(fg = 'green')
+    task_start_time = dt.time(0,0,0,0)
+    task_end_time = dt.time(0,0,0,0)
+
+    total_time_paused = dt.time(0,0,0,0)
+
+    csv_path = ''
 
 
-def end_task():
-    global start_time
-    global total_time_paused
-    global csv_path
-    stop['state'] = 'disabled'
-    start['state'] = 'active'
-    current_time.config(fg = 'red')
-    stop_time = dt.datetime.now()
-    stop_time = stop_time.replace(microsecond=0)
-    start_time = start_time.replace(microsecond=0)
-    print(start_time, stop_time)
-    today = dt.datetime.today()
-    time_taken = (stop_time - start_time) - total_time_paused
-    start_time = dt.datetime.now()
-    List = [name_entry.get(), today.strftime('%m/%d/%Y'), task_description_entry.get(), task_category_combobox.get(), str(time_taken)]
-    total_time_paused = dt.timedelta(seconds=0)
+    def __init__(self, ClockLabel, NameEntry, TaskDescEntry, TaskCategoryBox, StartButton, PauseButton, StopButton):
+        #Widgets
+        self.ClockLabel = ClockLabel
+        self.NameEntry = NameEntry
+        self.TaskDescEntry = TaskDescEntry
+        self.TaskCategoryBox = TaskCategoryBox
+        self.StartButton = StartButton
+        self.PauseButton = PauseButton
+        self.StopButton = StopButton
 
-    task_category_combobox['state'] = 'readonly'
-    task_description_entry['state'] = 'normal'
-    pause['state'] = 'disabled'
-    name_entry['state'] = 'normal'
+        self.return_current_time()
 
 
-    with open(csv_path, 'a') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',')
-        writer.writerow(List)
-        csvfile.close()
 
-def pause_task():
-    global total_time_paused
-    global is_paused
-    global start_pause_time
+    def return_current_time(self):
+        now = dt.datetime.now()
+        self.ClockLabel.config(text = now.strftime('%I:%M:%S %p'))
+        self.ClockLabel.after(1000, self.return_current_time)
 
-    if (is_paused):
-        end_pause_time = dt.datetime.now()
-        end_pause_time = end_pause_time.replace(microsecond=0)
-        total_time_paused += end_pause_time - start_pause_time
-        print(str(total_time_paused))
-        pause.config(text='Pause Task', bg='blue')
+    def begin_task(self):
+
+        #configure widgets for state (started task)
+        self.StartButton['state'] = 'disabled'
+        self.StopButton['state'] = 'active'
+        self.PauseButton['state'] = 'active'
+        self.TaskCategoryBox['state'] = 'disabled'
+        self.TaskDescEntry['state'] = 'disabled'
+        name_entry['state'] = 'disabled'
         current_time.config(fg = 'green')
-        is_paused = False
 
-    elif not(is_paused):
-        start_pause_time = dt.datetime.now()
-        start_pause_time = start_pause_time.replace(microsecond=0)
-        pause.config(text='Resume Task', bg='yellow')
-        current_time.config(fg = 'yellow')
-        is_paused = True
+        #sets the start time to when the user clicked the start task button
+        self.task_start_time = dt.datetime.now()
 
-def Find_File_Path():
-    global csv_path
-    if(os.path.exists('/Volumes/Watts Atelier’s Public Folder/TaskInfo/tasks.csv')):
-        csv_path = '/Volumes/Watts Atelier’s Public Folder/TaskInfo/tasks.csv'
-    elif(os.path.exists('/Users/wattsatelier/Public/TaskInfo/tasks.csv')):
-        csv_path ='/Users/wattsatelier/Public/TaskInfo/tasks.csv'
-    elif(os.path.exists('./taskstesting.csv')):
-        csv_path = './taskstesting.csv'
-    else:
-        print("Couldn't find the tasks file.")
-        window.config(bg='red')
+
+    def end_task(self):
+
+        #configure widgets for state (ended task) TODO: Split state configurations into their own method
+        self.StopButton['state'] = 'disabled'
+        self.StartButton['state'] = 'active'
+        self.ClockLabel.config(fg = 'red')
+        self.TaskCategoryBox['state'] = 'readonly'
+        self.TaskDescEntry['state'] = 'normal'
+        self.PauseButton['state'] = 'disabled'
+        self.NameEntry['state'] = 'normal'
+
+        self.task_end_time = dt.datetime.now()
+        today = dt.datetime.today()
+        
+        self.task_end_time = self.task_end_time.replace(microsecond=0)
+        self.task_start_time = self.task_start_time.replace(microsecond=0)
+        time_taken = (self.task_end_time - self.task_start_time) - self.total_time_paused
+        self.total_time_paused = dt.timedelta(seconds=0)
+
+        List = [self.NameEntry.get(), today.strftime('%m/%d/%Y'), self.TaskDescEntry.get(), self.TaskCategoryBox.get(), str(time_taken)]
+
+        #TODO: make writing to the CSV file it's own method.
+        with open(csv_path, 'a') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(List)
+            csvfile.close()
+
+    def pause_task():
+        global total_time_paused
+        global is_paused
+        global start_pause_time
+
+        if (is_paused):
+            end_pause_time = dt.datetime.now()
+            end_pause_time = end_pause_time.replace(microsecond=0)
+            total_time_paused += end_pause_time - start_pause_time
+            print(str(total_time_paused))
+            pause.config(text='Pause Task', bg='blue')
+            current_time.config(fg = 'green')
+            is_paused = False
+
+        elif not(is_paused):
+            start_pause_time = dt.datetime.now()
+            start_pause_time = start_pause_time.replace(microsecond=0)
+            pause.config(text='Resume Task', bg='yellow')
+            current_time.config(fg = 'yellow')
+            is_paused = True
+
+    def Find_File_Path():
+        global csv_path
+        if(os.path.exists('/Volumes/Watts Atelier’s Public Folder/TaskInfo/tasks.csv')):
+            csv_path = '/Volumes/Watts Atelier’s Public Folder/TaskInfo/tasks.csv'
+        elif(os.path.exists('/Users/wattsatelier/Public/TaskInfo/tasks.csv')):
+            csv_path ='/Users/wattsatelier/Public/TaskInfo/tasks.csv'
+        elif(os.path.exists('./taskstesting.csv')):
+            csv_path = './taskstesting.csv'
+        else:
+            print("Couldn't find the tasks file.")
+            window.config(bg='red')
 
 
 
